319ce043a0ff17185f04b6862599c9f20d3f1dac
Natasha Malik <nm240nm@gmail.com>
2019-09-03 16:53:28 +0530

    constants files

---
+++ src/com/safapharma/Helpers/Constants.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.safapharma.Helpers;
+
+/**
+ *
+ * @author Natasha Malik
+ */
+public class Constants {
+    
+    public static enum SCREEN_CREATION{FIRST_TIME_CREATION, ALREADY_CREATED};
+    public static final boolean VALID = true;
+    public static final boolean IN_VALID = true;
+    
+    
+}
