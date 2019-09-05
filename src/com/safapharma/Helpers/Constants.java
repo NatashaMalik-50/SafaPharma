de9383e2b3c68abfdae69b878d2a06bb4f1d1d66
Natasha Malik <nm240nm@gmail.com>
2019-09-06 03:44:03 +0530

    constants updated

--- src/com/safapharma/Helpers/Constants.java
+++ src/com/safapharma/Helpers/Constants.java
@@ -5,6 +5,7 @@
  */
 package com.safapharma.Helpers;
 
+import java.awt.Color;
 import java.awt.Font;
 
 /**
@@ -12,18 +13,18 @@ import java.awt.Font;
  * @author Natasha Malik
  */
 public class Constants {
-    
+
     public static final int VALID = 1;
     public static final int INVALID = -1;
-    public static final String BACK_LABEL = "Back";    
+    public static final String BACK_LABEL = "Back";
     public static final String LOG_OUT_LABEL = "Log Out";
     public static final String DATE_LABEL = "Date";
     public static final String DAY_LABEL = "Day";
-    public static final String TIME_LABEL = "Time";    
+    public static final String TIME_LABEL = "Time";
     public static final String SUPPLIER_LABEL = "SUPPLIER";
     public static final String SALES_LABEL = "SALES";
     public static final String HOMESCREEN_LABEL = "HOMESCREEN";
-    
+
     public static final String BUTTON_CUSTOMER_LABEL = "Customer";
     public static final String BUTTON_SALES_LABEL = "Sales";
     public static final String BUTTON_STOCK_LABEL = "Stock";
@@ -31,8 +32,7 @@ public class Constants {
     public static final String BUTTON_MEDICINE_LABEL = "Medicine";
     public static final String BUTTON_MEDICINE_LOT_LABEL = "Medicine Lot";
     public static final String BUTTON_BACKUP_LABEL = "Backup";
-    
-    
+
     public static final String TABLE_USERS = "user";
     public static final String TABLE_CUSTOMER = "customer";
     public static final String TABLE_MEDICINE = "medicine";
@@ -50,10 +50,10 @@ public class Constants {
     public static final String TABLE_MEDICINE_SALT = "medicine_salt";
     public static final String TABLE_ROLE = "role";
     public static final String TABLE_USER_ROLE = "user_role";
-    
-    public static final Font FONT_SIZE_16_CALIBRI = new Font("Calibri", Font.BOLD, 16);
-    public static final Font FONT_SIZE_14_CALIBRI = new Font("Calibri", Font.BOLD, 14);
-    
-    
-    
+    public static final String INVALID_PASSWORD_MSG = "Incorrect password. Kindly enter again.";
+    public static final String INVALID_USERNAME_MSG = "Incorrect username. Kindly enter again.";
+    public static final String LOGIN_ERROR_MSG = "Login Error";
+    public static final String EMPTY_LOGIN_FIELDS = "Username or password cannot be empty. Kindly enter again.";
+
+
 }
