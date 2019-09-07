5f97e52ecfd8e3ca73d78db67750432692545bd8
Natasha Malik <nm240nm@gmail.com>
2019-09-07 18:11:05 +0530

    templates added

---
+++ src/com/safapharma/Home/NewStockViewForm.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.safapharma.Home;
+
+import com.safapharma.Main.MainWindow;
+import com.safapharma.Templates.DialogForm;
+import java.awt.Frame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Natasha Malik
+ */
+public class NewStockViewForm extends DialogForm {
+
+    private MainWindow manager;
+    private FormLabel nameLabel;
+    private FormText nameText;
+    private FormLabel contactLabel;
+    private FormText contactText;
+    private FormButton submitButton;
+    private FormButton resetButton;
+
+    public NewStockViewForm(MainWindow manager) {
+        this.manager = manager;
+        initUI();
+    }
+
+    private void initUI() {
+        getFormLabel().setText("Create New Stock View");
+        nameLabel = new FormLabel("Name");
+        nameText = new FormText();
+        contactLabel = new FormLabel("Contact No.");
+        contactText = new FormText();
+        submitButton = new FormButton("Submit");
+        resetButton = new FormButton("Reset");
+
+        getFormPanel().add(nameLabel);
+        getFormPanel().add(nameText);
+        getFormPanel().add(contactLabel);
+        getFormPanel().add(contactText);
+        getFormPanel().add(submitButton);
+        getFormPanel().add(resetButton);
+        
+    }
+
+}
