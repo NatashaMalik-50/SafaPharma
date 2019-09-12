14e4be7aa656c29b1e3ea64cc322034b1162511e
Natasha Malik <nm240nm@gmail.com>
2019-09-12 10:01:26 +0530

    stored id data of billing entries

--- src/com/safapharma/Home/HomeScreenPanel.java
+++ src/com/safapharma/Home/HomeScreenPanel.java
@@ -45,25 +45,25 @@ public class HomeScreenPanel extends MainScreenPanel {
     private final HomeScreenBackend homeScreenBackend;
     private TableRowSorter sorter;
     private DataWithColumn stockData;
+    private DataWithColumn billData;
     private final HomeScreenPanel thisHome;
-    private Object[] column = {"Sr.No","Medicine Name","Quantity","Supplier Name","Rate"};
+    private Object[] column = {"Sr.No", "Medicine Name", "Quantity", "Supplier Name", "Rate"};
     private JLabel totalLable;
     private JLabel totalBox;
     protected ToolbarButton btnAddCustomer;
     private ToolbarButton btnGenerateBill;
-    
 
     public HomeScreenPanel(MainWindow manager) {
         this.manager = manager;
         this.thisHome = this;
         homeScreenBackend = new HomeScreenBackend();
-        
+
         initUI();
         setListeners();
     }
 
     private void initUI() {
-        
+
         tableModel = new CustomDefaultTableModel();
         new SwingWorker<Void, Void>() {
             @Override
@@ -75,22 +75,21 @@ public class HomeScreenPanel extends MainScreenPanel {
         billEntriesTable = new JTable(tableModel);
         sorter = new TableRowSorter(tableModel);
         billEntriesTable.setRowSorter(sorter);
-        
+
         billEntriesTable.getTableHeader().setResizingAllowed(false);
         billEntriesTable.getTableHeader().setFont(DesignConstants.FONT_SIZE_14_CALIBRI_BOLD);
         billEntriesTable.setFont(DesignConstants.FONT_SIZE_14_CALIBRI);
         billEntriesTable.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
         getTableScrollPane().setViewportView(billEntriesTable);
-        
+
         totalLable = new JLabel();
         totalLable.setText("Total :");
         totalLable.setFont(DesignConstants.FONT_SIZE_18_CALIBRI_BOLD);
-        
+
         totalBox = new JLabel();
-        totalBox.setText("₹ "+"0.0");
+        totalBox.setText("₹ " + "0.0");
         totalBox.setFont(DesignConstants.FONT_SIZE_18_CALIBRI_BOLD);
-        
-        
+
         btnGenerateBill = new ToolbarButton("Bill");
         btnAddCustomer = new ToolbarButton("AddCustomer");
         //btnAddCustomer = new ToolbarButton("Search",new ImageIcon(getClass().getResource(IconConstants.SEARCH_ICON)));
@@ -99,18 +98,19 @@ public class HomeScreenPanel extends MainScreenPanel {
         btnGenerateBill.setEnabled(false);
         //TotalBox.
         getTotalPanel().setLayout(new GridLayout(0, 2));
-        
+
         getTotalPanel().add(totalLable);
         getTotalPanel().add(totalBox);
         getTotalPanel().add(btnAddCustomer);
         getTotalPanel().add(btnGenerateBill);
 
+        billData = new DataWithColumn(true);
+
     }
 
     private void loadStockData() throws Exception {
-        
-       
-        stockData = homeScreenBackend.setStockInfoIntoTable();             
+
+        stockData = homeScreenBackend.setStockInfoIntoTable();
         //tableModel.setDataVector(null, stockData.getColumnNames());
         tableModel.setColumnIdentifiers(column);
         billEntriesTable.setModel(tableModel);
@@ -133,7 +133,7 @@ public class HomeScreenPanel extends MainScreenPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    if(stockData==null){
+                    if (stockData == null) {
                         setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));
                         loadStockData();
                         setCursor(Cursor.getDefaultCursor());
@@ -149,34 +149,30 @@ public class HomeScreenPanel extends MainScreenPanel {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                   int id = billEntriesTable.getSelectedRow();
-                   System.out.println(id);
-                   //billEntriesTable.remove(id);
-                   if(billEntriesTable.getSelectedRow()>=0)
-                   {
-                    DefaultTableModel model = (DefaultTableModel) billEntriesTable.getModel();
-                    model.removeRow(id);
-                    if(billEntriesTable.getRowCount()>=0)
-                    {
-                       disableRemoveButtons();
-                       disableUpdateButtons();
-                       disableViewButtons();
+                    int id = billEntriesTable.getSelectedRow();
+                    System.out.println(id);
+                    //billEntriesTable.remove(id);
+                    if (billEntriesTable.getSelectedRow() >= 0) {
+                        DefaultTableModel model = (DefaultTableModel) billEntriesTable.getModel();
+                        model.removeRow(id);
+                        if (billEntriesTable.getRowCount() >= 0) {
+                            disableRemoveButtons();
+                            disableUpdateButtons();
+                            disableViewButtons();
+                        }
+                    } else {
+                        disableRemoveButtons();
+                        disableUpdateButtons();
+                        disableViewButtons();
                     }
-                   }
-                   else
-                   {
-                       disableRemoveButtons();
-                       disableUpdateButtons();
-                       disableViewButtons();
-                   }
-                   getSum();
+                    getSum();
                 } catch (Exception ex) {
                     Logger.getLogger(HomeScreenPanel.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                
+
             }
         });
-            btnAddCustomer.addActionListener(new ActionListener() {
+        btnAddCustomer.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
@@ -187,15 +183,14 @@ public class HomeScreenPanel extends MainScreenPanel {
                 }
             }
         });
-        billEntriesTable.addMouseListener(new MouseAdapter(){
+        billEntriesTable.addMouseListener(new MouseAdapter() {
             @Override
-            public void mouseClicked(MouseEvent e)
-            {
+            public void mouseClicked(MouseEvent e) {
                 enableRemoveButtons();
                 enableUpdateButtons();
                 enableViewButtons();
             }
-    });
+        });
 
         searchBox.getDocument().addDocumentListener(new DocumentListener() {
             @Override
@@ -217,50 +212,53 @@ public class HomeScreenPanel extends MainScreenPanel {
                 if (str.length() == 0) {
                     sorter.setRowFilter(null);
                 } else {
-                    sorter.setRowFilter(RowFilter.regexFilter("(?i)" +str));
+                    sorter.setRowFilter(RowFilter.regexFilter("(?i)" + str));
                 }
             }
         });
     }
-    
+
     public DataWithColumn getStockData() {
         return stockData;
     }
-    public void getSum()
-    {
+
+    public void getSum() {
         float sum1 = 0;
-        for(int i = 0; i < billEntriesTable.getRowCount(); i++)
-        { 
-            try{
+        for (int i = 0; i < billEntriesTable.getRowCount(); i++) {
+            try {
                 sum1 = sum1 + Float.parseFloat(billEntriesTable.getValueAt(i, 4).toString());
-             }
-            catch(Exception e){
-               java.util.logging.Logger.getLogger(HomeScreenPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, e);
-                }
-        }    
-         totalBox.setText("₹ " + Float.toString(sum1));
-         totalBox.setHorizontalAlignment(JTextField.CENTER);
+            } catch (Exception e) {
+                java.util.logging.Logger.getLogger(HomeScreenPanel.class.getName()).log(java.util.logging.Level.SEVERE, null, e);
+            }
+        }
+        totalBox.setText("₹ " + Float.toString(sum1));
+        totalBox.setHorizontalAlignment(JTextField.CENTER);
     }
-    
-    public void addRow(int rowIndex){
+
+    public void addRow(int rowIndex) {
         //System.out.println("found row : "+rowIndex+ " with value"+stockData.getDataOf(rowIndex)+ " --with id-- "+stockData.getIdData());
-        
-      
+
         DefaultTableModel model = (DefaultTableModel) billEntriesTable.getModel();
-        Object[] ob = new Object[5];
-        ob[0]=stockData.getDataOf(rowIndex-1).get(0).toString();
-        ob[1]=stockData.getDataOf(rowIndex-1).get(2).toString();
-        ob[2]=stockData.getDataOf(rowIndex-1).get(3).toString();
-        ob[3]=stockData.getDataOf(rowIndex-1).get(7).toString();
-        ob[4]=stockData.getDataOf(rowIndex-1).get(4).toString();
+        Vector<Object> newRow = new Vector<>();
+        newRow.add(stockData.getDataOf(rowIndex - 1).get(0).toString());
+        newRow.add(stockData.getDataOf(rowIndex - 1).get(2).toString());
+        newRow.add(stockData.getDataOf(rowIndex - 1).get(3).toString());
+        newRow.add(stockData.getDataOf(rowIndex - 1).get(7).toString());
+        newRow.add(stockData.getDataOf(rowIndex - 1).get(4).toString());
 //        ob[0]=billEntriesTable.getValueAt(rowIndex-1, 0).toString();
 //        ob[1]=billEntriesTable.getValueAt(rowIndex-1, 2).toString();
 //        ob[2]=billEntriesTable.getValueAt(rowIndex-1, 3).toString();
 //        ob[3]=billEntriesTable.getValueAt(rowIndex-1, 7).toString();
 //        ob[4]=billEntriesTable.getValueAt(rowIndex-1, 4).toString();
         //model.addRow(stockData.getDataOf(rowIndex-1));
-        model.addRow(ob);
-        
+        model.addRow(newRow);
+        //bill row also stores id of the object
+        Vector<Object> billRow = new Vector<>();
+        billRow.add(stockData.getIdBySerialNo(Integer.parseInt((String)newRow.get(0))));
+        billRow.addAll(newRow);
+        billData.getData().add(billRow);
+        System.out.print("Vec -- "+billData.getData());
+
         /*Vector v = new Vector();
         v.add("Sr.No");
         v.add("Medicine Name");
@@ -271,10 +269,8 @@ public class HomeScreenPanel extends MainScreenPanel {
         
         DefaultTableModel model = new DefaultTableModel();
         model.setDataVector(ob, v);
-        */
-        
-         
+         */
         getSum();
-        
+
     }
 }
