1caeea9bca9b98c6a2c1f3c832fdf0a15bdb8275
Karan <karan07.nagpal@gmail.com>
2019-09-14 23:01:42 +0530

    Added validations in NewStockFormTwo and add suppliername,medicinelot works for now

--- src/com/safapharma/Stock/NewStockFormTwo.java
+++ src/com/safapharma/Stock/NewStockFormTwo.java
@@ -22,6 +22,7 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import org.jdesktop.swingx.autocomplete.AutoCompleteDecorator;
 import com.safapharma.ModelObjects.MedicineLot;
+import java.awt.GridBagLayout;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.BoxLayout;
@@ -29,6 +30,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.SwingWorker;
 import javax.swing.table.DefaultTableModel;
+import jdk.nashorn.internal.runtime.JSType;
 
 /**
  *
@@ -40,7 +42,7 @@ public class NewStockFormTwo extends DialogForm {
     
     private FormLabel supplierNameLabel;
     private JComboBox supplierNameCombo;
-//    private ErrorLabel supplierNameErrorLabel;
+    private ErrorLabel combinedErrorLabel;
     private FormButton supplierNameAddButton;
     
         
@@ -81,6 +83,10 @@ public class NewStockFormTwo extends DialogForm {
     
     private JPanel upperPanel;
     private JPanel lowerPanel;
+    private JPanel buttonPanel;            
+    
+    private FormButton submitStock;
+    private FormButton deleteStock;
     
     private JTable stockTable;
     private DefaultTableModel tableModel;
@@ -113,16 +119,16 @@ public class NewStockFormTwo extends DialogForm {
         
         supplierNameLabel = new FormLabel("Supplier Name");
         supplierNameCombo = new JComboBox(fullSupplierNameList);
-        supplierNameAddButton = new FormButton("Add");
+        supplierNameAddButton = new FormButton("Add Supplier Name");
         
         medicineNameLabel = new FormLabel("Medicine Name");
         medicineNameCombo = new JComboBox(fullNameList);
-        medicineNameAddButton = new FormButton("Add");
+        medicineNameAddButton = new FormButton("Add Medicine Name");
         medicineNameErrorLabel = new ErrorLabel();
         
         medicineLotBatchLabel = new FormLabel("Medicine Lot Batch");
         medicineLotBatchCombo = new JComboBox(fullBatchNameList);
-        medicineLotBatchButton = new FormButton("Add");
+        medicineLotBatchButton = new FormButton("Add Medicine Lot Batch");
         
 
         quantityLabel = new FormLabel("Quantity");
@@ -143,8 +149,19 @@ public class NewStockFormTwo extends DialogForm {
         
         submitButton = new FormButton("Submit");
         resetButton = new FormButton("Reset");
+        combinedErrorLabel = new ErrorLabel();
+        
+        submitStock = new FormButton("Submit Lot");
+        deleteStock = new FormButton("Delete Selected Lot");
+       
+        lowerPanel = new JPanel();
+        lowerPanel.setLayout(new BoxLayout(lowerPanel,BoxLayout.Y_AXIS));
+        buttonPanel = new JPanel(new GridLayout(0,2));
         
         setComboBox(medicineNameCombo,supplierNameCombo,medicineLotBatchCombo);
+        
+        buttonPanel.add(submitStock);
+        buttonPanel.add(deleteStock);
         
         upperPanel.add(supplierNameLabel);
         upperPanel.add(supplierNameCombo);
@@ -180,10 +197,7 @@ public class NewStockFormTwo extends DialogForm {
 
         upperPanel.add(submitButton);
         upperPanel.add(resetButton);
-        
-
-        getFormPanel().add(upperPanel);
-        
+        upperPanel.add(combinedErrorLabel);        
         
 //        getButtonPanel().add(submitButton);
 //        getButtonPanel().add(resetButton,getGBC(1));
@@ -210,7 +224,13 @@ public class NewStockFormTwo extends DialogForm {
         }.execute();
         
         scrollPane.setViewportView(stockTable);
-        getFormPanel().add(scrollPane);
+        
+        lowerPanel.add(scrollPane);
+        lowerPanel.add(buttonPanel);
+        
+        getFormPanel().add(upperPanel);
+        getFormPanel().add(lowerPanel);
+        
 
         this.pack();
         hideErrorLabels();
@@ -287,12 +307,22 @@ public class NewStockFormTwo extends DialogForm {
         DefaultTableModel model = (DefaultTableModel) stockTable.getModel();
         Vector<Object> newRow = new Vector<>();
         
+        String qtyString = this.quantityText.getText();
+        String discountPercentageString = this.discountPercentageText.getText();
+        String gstString = this.gstText.getText();
+        String amountString = this.amountText.getText();
+        
+        int qty = Integer.parseInt(qtyString);
+        double discountPercentage = Double.parseDouble(discountPercentageString);
+        double gst = Double.parseDouble(gstString);
+        double amount = Double.parseDouble(amountString);                    
+    
         newRow.add(medicineNameCombo.getSelectedItem().toString());
         newRow.add(medicineLotBatchCombo.getSelectedItem().toString());
-        newRow.add(quantityText.getText());
-        newRow.add(discountPercentageText.getText());
-        newRow.add(gstText.getText());
-        newRow.add(amountText.getText());
+        newRow.add(qty);
+        newRow.add(discountPercentage);
+        newRow.add(gst);
+        newRow.add(amount);
         
         model.addRow(newRow);
     }
@@ -319,8 +349,10 @@ public class NewStockFormTwo extends DialogForm {
         submitButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {              
-                addRow();             
-                // Karan here
+                if(validateInfo()){
+                    addRow();             
+                    
+                }
             }
         });
         resetButton.addActionListener(new ActionListener() {
@@ -349,6 +381,48 @@ public class NewStockFormTwo extends DialogForm {
                 
             }
         });
+        
+        medicineLotBatchButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Add Medicine Lot Batch Called");
+                try {
+                    manager.createNewMedicineLotForm(new MedicineLotBackend(), null);
+                    manager.showNewMedicineLotForm();
+                } catch (Exception ex) {
+                    Logger.getLogger(NewStockFormTwo.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                
+            }
+        });
+        
+        
+        submitStock.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {              
+                System.out.println("Submit Stock button clicked");
+                
+                DefaultTableModel model = (DefaultTableModel) stockTable.getModel();                                
+                System.out.println("All data in add stock table");
+                
+                Vector<Vector>  submitLotData = model.getDataVector();
+                for(Vector object : submitLotData){
+                    System.out.println(object);
+                }
+            }
+        });
+        
+        deleteStock.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {              
+                System.out.println("Delete Selected Stock button clicked");
+                DefaultTableModel model = (DefaultTableModel) stockTable.getModel();
+                int numRows = stockTable.getSelectedRows().length;                
+                for(int i=0; i<numRows; i++) {
+                    model.removeRow(stockTable.getSelectedRow());
+                }
+            }
+        });
     }
     
     private boolean isEmpty(String str){
@@ -363,32 +437,113 @@ public class NewStockFormTwo extends DialogForm {
     private boolean validateInfo() {
         resetErrorLabels();
         boolean isValid = true;
+        String errorMsg = "Please select ";
         try{
         
             /*Checking if the respective textfields are empty or not. */
+            if(supplierNameCombo.getSelectedItem().toString().isEmpty()){
+                isValid = false;
+                errorMsg = errorMsg + "supplier name,";
+                combinedErrorLabel.setText(errorMsg);
+            }
+            
             if(medicineNameCombo.getSelectedItem().toString().isEmpty()){
                 isValid = false;
-                medicineNameErrorLabel.setText("Incorrect Medicine Name");
+                errorMsg = errorMsg + "medicine name,";
+                combinedErrorLabel.setText(errorMsg);
             }
+            
+            if(medicineLotBatchCombo.getSelectedItem().toString().isEmpty()){
+                isValid = false;
+                errorMsg = errorMsg + "medicine lot batch";
+                combinedErrorLabel.setText(errorMsg);
+            }
+            
             if(quantityText.getText().isEmpty()){
                 isValid = false;
-                quantityErrorLabel.setText("Incorrect batch number");
+                quantityErrorLabel.setText("Quantity cannot be empty");
             }
                 
             if(discountPercentageText.getText().isEmpty()){
                 isValid = false;
-                discountPercentageErrorLabel.setText("Incorrect Expiry ");
+                discountPercentageErrorLabel.setText("Discount cannot be empty");
             }
             if(gstText.getText().isEmpty()){
                 isValid = false;
-                gstErrorLabel.setText("Incorrect Rate");
+                gstErrorLabel.setText("GST cannot be empty");
             }
+            if(amountText.getText().isEmpty()){
+                isValid = false;
+                amountErrorLabel.setText("Amount cannot be empty");
+            }
+            
+            
             if(isValid){
-                double num = Double.parseDouble(gstText.getText());
-                if(num<=0){
+                String qtyString = this.quantityText.getText();
+                String discountPercentageString = this.discountPercentageText.getText();
+                String gstString = this.gstText.getText();
+                String amountString = this.amountText.getText();
+                
+                int qty=0;
+                double discountPercentage = 0.0;
+                double gst = 0.0 ;
+                double amount = 0.0;
+                
+                if(isInteger(qtyString)){
+                    qty = Integer.parseInt(qtyString);
+                }
+                else{
+                    this.quantityErrorLabel.setText("Quantity should be a valid number.");
                     isValid = false;
                 }
-            }
+                
+                if(isDouble(discountPercentageString)){
+                    discountPercentage = Double.parseDouble(discountPercentageString);
+                }
+                else{
+                    this.discountPercentageErrorLabel.setText("Discount Percentage should be a valid number.");
+                    isValid = false;
+                }
+
+                if(isDouble(gstString)){
+                    gst = Double.parseDouble(gstString);
+                }
+                else{
+                    this.gstErrorLabel.setText("GST Percentage should be a valid number.");
+                    isValid = false;
+                }
+                
+                if(isDouble(amountString)){
+                    amount = Double.parseDouble(amountString);                    
+                }
+                else{
+                    this.amountErrorLabel.setText("Amount should be a valid number.");
+                    isValid = false;
+                }
+                
+                if(isValid){
+                    if(qty<0)
+                    {
+                        this.quantityErrorLabel.setText("Quantity cannot be less than 0");
+                        isValid = false;
+                    }
+                    
+                    if(discountPercentage<0.0){
+                        this.discountPercentageErrorLabel.setText("Discount Percentage cannot be less than 0");
+                        isValid = false;
+                                
+                    }
+                    if(gst<0.0){
+                        this.gstErrorLabel.setText("Discount Percentage cannot be less than 0");
+                        isValid = false;
+                                
+                    }
+                    if(amount<0.0){
+                        this.amountErrorLabel.setText("Discount Percentage cannot be less than 0");
+                        isValid = false;                                
+                    }
+                }                
+            }               
             
             if(isValid){
                resetErrorLabels();
@@ -405,11 +560,25 @@ public class NewStockFormTwo extends DialogForm {
             return false;
         }
     }
-
-//    private Supplier generateSupplier() {
-//        Supplier supplier = new Supplier(nameText.getText(), addressText.getText(), contactText.getText(), emailText.getText());
-//        return supplier;
-//    }
+    
+    boolean isDouble(String str) {
+        try {
+            Float.parseFloat(str);
+            Double.parseDouble(str);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+    
+    boolean isInteger(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
 
     private void hideErrorLabels() {
         medicineNameErrorLabel.setVisible(false);
@@ -417,6 +586,7 @@ public class NewStockFormTwo extends DialogForm {
         discountPercentageErrorLabel.setVisible(false);
         gstErrorLabel.setVisible(false);
         amountErrorLabel.setVisible(false);
+        combinedErrorLabel.setVisible(false);
     }
     
     private void showErrorLabels() {
@@ -424,7 +594,8 @@ public class NewStockFormTwo extends DialogForm {
         quantityErrorLabel.setVisible(true);
         discountPercentageErrorLabel.setVisible(true);
         gstErrorLabel.setVisible(true);
-        amountErrorLabel.setVisible(false);
+        amountErrorLabel.setVisible(true);
+        combinedErrorLabel.setVisible(true);
     }
 
     private void resetErrorLabels() {
@@ -433,6 +604,7 @@ public class NewStockFormTwo extends DialogForm {
         discountPercentageErrorLabel.setErrorText("");
         gstErrorLabel.setErrorText("");
         amountErrorLabel.setErrorText("");
+        combinedErrorLabel.setErrorText("");
         hideErrorLabels();
     }
     
